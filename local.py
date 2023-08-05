96c5b5fe577161e9a6e461c612c7655486b224bc
michael <michael.norton1213@gmail.com>
2023-08-05 19:30:03 -0400

    create db

---
+++ local.py
@@ -0,0 +1,1 @@
+sqlpassword = "Mypass@22"
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from sqlconnect import create_server_connection
 from local import sqlpassword
+from createdb import create_database
 
 
 connection = create_server_connection("localhost","root",sqlpassword)
+dbquery = "CREATE DATABASE school"
+
+create_database(connection,dbquery)
+
+
