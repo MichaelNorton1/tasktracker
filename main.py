3fe823fbd264cf25677fa306e08b48266803c2c2
michael <michael.norton1213@gmail.com>
2023-08-05 19:19:32 -0400

    ignore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sqlconnect import create_server_connection
+from local import sqlpassword
 
 
-connection = create_server_connection("localhost","root","")
+connection = create_server_connection("localhost","root",sqlpassword)
