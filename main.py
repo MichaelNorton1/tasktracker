5109fc062ace2e928b6cb010f58011567bfaa8fe
michael <michael.norton1213@gmail.com>
2023-08-05 20:12:46 -0400

    create other tables

--- main.py
+++ main.py
@@ -4,24 +4,50 @@ from query_db import execute_query
 from createdb import create_database
 
 dbname="school"
-create_teacher_table = """
-CREATE TABLE teacher (
-  teacher_id INT PRIMARY KEY,
-  first_name VARCHAR(40) NOT NULL,
-  last_name VARCHAR(40) NOT NULL,
-  language_1 VARCHAR(3) NOT NULL,
-  language_2 VARCHAR(3),
-  dob DATE,
-  tax_id INT UNIQUE,
-  phone_no VARCHAR(20)
-  );
- """
+
 
 connection = create_db_connection("localhost","root",sqlpassword,dbname)
 
-execute_query(connection,create_teacher_table)
 
 
 
+create_client_table = """
+CREATE TABLE client (
+  client_id INT PRIMARY KEY,
+  client_name VARCHAR(40) NOT NULL,
+  address VARCHAR(60) NOT NULL,
+  industry VARCHAR(20)
+);
+ """
+
+create_participant_table = """
+CREATE TABLE participant (
+  participant_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  phone_no VARCHAR(20),
+  client INT
+);
+"""
+
+create_course_table = """
+CREATE TABLE course (
+  course_id INT PRIMARY KEY,
+  course_name VARCHAR(40) NOT NULL,
+  language VARCHAR(3) NOT NULL,
+  level VARCHAR(2),
+  course_length_weeks INT,
+  start_date DATE,
+  in_school BOOLEAN,
+  teacher INT,
+  client INT
+);
+"""
+
+
+
+execute_query(connection, create_client_table)
+execute_query(connection, create_participant_table)
+execute_query(connection, create_course_table)
 
 
