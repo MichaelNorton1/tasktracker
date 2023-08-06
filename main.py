a78090587beb56329d459399f26ca2f80e17c72a
michael <michael.norton1213@gmail.com>
2023-08-05 20:07:18 -0400

    create teacher table

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from sqlconnect import create_server_connection
+from sqlconnect import create_db_connection
 from local import sqlpassword
+from query_db import execute_query
 from createdb import create_database
 
+dbname="school"
+create_teacher_table = """
+CREATE TABLE teacher (
+  teacher_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  language_1 VARCHAR(3) NOT NULL,
+  language_2 VARCHAR(3),
+  dob DATE,
+  tax_id INT UNIQUE,
+  phone_no VARCHAR(20)
+  );
+ """
 
-connection = create_server_connection("localhost","root",sqlpassword)
-dbquery = "CREATE DATABASE school"
+connection = create_db_connection("localhost","root",sqlpassword,dbname)
 
-create_database(connection,dbquery)
+execute_query(connection,create_teacher_table)
+
+
+
 
 
--- sqlconnect.py
+++ sqlconnect.py
@@ -3,15 +3,16 @@ from mysql.connector import Error
 import pandas as pd
 
 
-def create_server_connection(host_name, user_name, user_password):
+def create_db_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password
+            passwd=user_password,
+            database=db_name
         )
-        print("MySQL Database connection successful")
+        print("MySQL Database connection successful to",db_name)
     except Error as err:
         print(f"Error: '{err}'")
 
