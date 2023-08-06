70df5eb921a1b3e0d8ad55979109162812f27255
michael <michael.norton1213@gmail.com>
2023-08-06 10:34:50 -0400

    add foriegn keys

--- main.py
+++ main.py
@@ -11,43 +11,40 @@ connection = create_db_connection("localhost","root",sqlpassword,dbname)
 
 
 
-create_client_table = """
-CREATE TABLE client (
-  client_id INT PRIMARY KEY,
-  client_name VARCHAR(40) NOT NULL,
-  address VARCHAR(60) NOT NULL,
-  industry VARCHAR(20)
-);
- """
+alter_participant = """
+ALTER TABLE participant
+ADD FOREIGN KEY(client)
+REFERENCES client(client_id)
+ON DELETE SET NULL;
+"""
 
-create_participant_table = """
-CREATE TABLE participant (
-  participant_id INT PRIMARY KEY,
-  first_name VARCHAR(40) NOT NULL,
-  last_name VARCHAR(40) NOT NULL,
-  phone_no VARCHAR(20),
-  client INT
+alter_course = """
+ALTER TABLE course
+ADD FOREIGN KEY(teacher)
+REFERENCES teacher(teacher_id)
+ON DELETE SET NULL;
+"""
+
+alter_course_again = """
+ALTER TABLE course
+ADD FOREIGN KEY(client)
+REFERENCES client(client_id)
+ON DELETE SET NULL;
+"""
+
+create_takescourse_table = """
+CREATE TABLE takes_course (
+  participant_id INT,
+  course_id INT,
+  PRIMARY KEY(participant_id, course_id),
+  FOREIGN KEY(participant_id) REFERENCES participant(participant_id) ON DELETE CASCADE,
+  FOREIGN KEY(course_id) REFERENCES course(course_id) ON DELETE CASCADE
 );
 """
 
-create_course_table = """
-CREATE TABLE course (
-  course_id INT PRIMARY KEY,
-  course_name VARCHAR(40) NOT NULL,
-  language VARCHAR(3) NOT NULL,
-  level VARCHAR(2),
-  course_length_weeks INT,
-  start_date DATE,
-  in_school BOOLEAN,
-  teacher INT,
-  client INT
-);
-"""
 
-
-
-execute_query(connection, create_client_table)
-execute_query(connection, create_participant_table)
-execute_query(connection, create_course_table)
-
+execute_query(connection, alter_participant)
+execute_query(connection, alter_course)
+execute_query(connection, alter_course_again)
+execute_query(connection, create_takescourse_table)
 
